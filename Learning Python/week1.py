835dca85501730b2d56d85c131a0bd7d9ef04194
Harrison Gatt <gattha@shckyneton.lan>
2021-02-22 15:01:47 +1100

    added week1 work

---
+++ Learning Python/week1.py
@@ -0,0 +1,12 @@
+# sets x to 5
+x = 5
+
+# tests if x is 7
+if x == 7:
+    # if x is 7 test if it is less than 10
+    if x < 10:
+        # shows the text in the console
+        print("X equal to 7 and 10")
+else:
+    # if x is not 7 then show this text in the console
+    print("X not equal to 7")  
