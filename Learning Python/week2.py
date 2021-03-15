c4a29629fc7fbb220efce341fea99ece68095860
Harrison Gatt <gattha@shckyneton.lan>
2021-03-15 14:22:21 +1100

    Create week2.py

---
+++ Learning Python/week2.py
@@ -0,0 +1,19 @@
+x = "+"
+a = 9
+b = 42.5
+
+if x == "+":
+    c = a + b
+    print(c)
+
+if x == "-":
+    c = a - b
+    print(c)
+
+if x == "*":
+    c = a * b
+    print(c)
+
+if x == "/":
+    c = a / b
+    print(c)
